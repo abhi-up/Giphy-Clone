43aa7b246161b50c4b0cddb95323dc992b7ff6e5
Abhi <meabhishek1512@gmail.com>
2024-05-16 21:51:47 +0530

    connected to gif api

--- src/context/gif-context.jsx
+++ src/context/gif-context.jsx
@@ -1,9 +1,33 @@
-import { createContext } from "react"
+import { GiphyFetch } from "@giphy/js-fetch-api"
+import { createContext, useContext, useState } from "react"
 
 const GifContext = createContext()
 
 const GifProvider = ({ children }) => {
-    return <GifContext.Provider>{children}</GifContext.Provider>
+    const [gifs, setGifs] = useState([])
+    const [filter, setFilter] = useState("gifs")
+    const [favorites, setFavorites] = useState([])
+
+    const gf = new GiphyFetch(import.meta.env.VITE_GIPHY_KEY)
+
+    return (
+        <GifContext.Provider
+            value={{
+                gf,
+                gifs,
+                setGifs,
+                filter,
+                setFilter,
+                favorites,
+            }}
+        >
+            {children}
+        </GifContext.Provider>
+    )
+}
+
+export const GifState = () => {
+    return useContext(GifContext)
 }
 
 export default GifProvider
