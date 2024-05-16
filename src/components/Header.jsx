4a9a624fa87419ea16bbb4683264e054e33d8339
Abhi <meabhishek1512@gmail.com>
2024-05-16 23:50:42 +0530

    rendered gifs categories

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { HiEllipsisVertical, HiMiniBars3BottomRight } from "react-icons/hi2"
 import { Link } from "react-router-dom"
 import { GifState } from "../context/gif-context"
@@ -8,6 +8,15 @@ const Header = () => {
     const [showCategories, setShowCategories] = useState(false)
 
     const { gf, filter, setFilter, favorites } = GifState()
+
+    const fetchGifCategories = async () => {
+        const { data } = await gf.categories()
+        setCategories(data)
+    }
+
+    useEffect(() => {
+        fetchGifCategories()
+    }, [])
 
     return (
         <nav>
@@ -21,9 +30,17 @@ const Header = () => {
 
                 <div className="font-bold text-md flex gap-2 items-center">
                     {/* render categories */}
-                    <Link className="px-4 py-1 hover:gradient border-b-4 hidden lg:block">
-                        Reactions
-                    </Link>
+                    {categories?.slice(0, 5)?.map((category) => {
+                        return (
+                            <Link
+                                key={category.name}
+                                to={`/${category.name_encoded}`}
+                                className="px-4 py-1 hover:gradient border-b-4 hidden lg:block"
+                            >
+                                {category.name}
+                            </Link>
+                        )
+                    })}
                     <button onClick={() => setShowCategories(!showCategories)}>
                         <HiEllipsisVertical
                             size={35}
@@ -33,9 +50,11 @@ const Header = () => {
                         />
                     </button>
 
-                    <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
-                        <Link to="/favorites">Favorite GIFs</Link>
-                    </div>
+                    {favorites.length > 0 && (
+                        <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
+                            <Link to="/favorites">Favorite GIFs</Link>
+                        </div>
+                    )}
 
                     <button>
                         <HiMiniBars3BottomRight
@@ -52,7 +71,17 @@ const Header = () => {
                         </span>
                         <hr className="bg-gray-100 opacity-50 my-5" />
                         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
-                            <Link className="font-bold">Reactions</Link>
+                            {categories?.map((category) => {
+                                return (
+                                    <Link
+                                        key={category.name}
+                                        to={`/${category.name_encoded}`}
+                                        className="font-bold"
+                                    >
+                                        {category.name}
+                                    </Link>
+                                )
+                            })}
                         </div>
                     </div>
                 )}
