e3f979fca0351007783e77b63dd54b9b0233320f
Abhi <meabhishek1512@gmail.com>
2024-05-16 19:48:28 +0530

    react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 import React from "react"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import AppLayout from "./layouts/AppLayout"
+import Home from "./pages/Home"
+import Categories from "./pages/Categories"
+import Search from "./pages/Search"
+import SingleGif from "./pages/SingleGif"
+import Favorites from "./pages/Favorites"
+
+const router = createBrowserRouter([
+    {
+        element: <AppLayout />,
+
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/:category",
+                element: <Categories />,
+            },
+            {
+                path: "/search/:query",
+                element: <Search />,
+            },
+            {
+                path: "/:type/:slug",
+                element: <SingleGif />,
+            },
+            {
+                path: "/favorites",
+                element: <Favorites />,
+            },
+        ],
+    },
+])
 
 const App = () => {
-    return <div>App</div>
+    return <RouterProvider router={router} />
 }
 
 export default App
