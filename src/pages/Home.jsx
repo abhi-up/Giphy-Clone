b26d687bc50b727c814b0f6e1e742d1434036cc1
Abhi <meabhishek1512@gmail.com>
2024-05-19 22:48:31 +0530

    build trending gifs page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,40 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { GifState } from "../context/gif-context"
+import Gif from "../components/Gif"
 
 const Home = () => {
-    return <div>Home</div>
+    const { gf, gifs, setGifs, filter } = GifState()
+
+    const fetchTrendingGIFs = async () => {
+        const { data } = await gf.trending({
+            limit: 20,
+            type: filter,
+            rating: "g",
+        })
+        setGifs(data)
+    }
+
+    useEffect(() => {
+        fetchTrendingGIFs()
+    }, [filter])
+
+    return (
+        <div className="">
+            <img
+                src="/banner.gif"
+                alt="earth banner"
+                className="mt-2 rounded w-full"
+            />
+
+            {/* <FilterGif showTrending /> */}
+
+            <div className="columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-2">
+                {gifs.map((gif) => (
+                    <Gif gif={gif} key={gif.id} />
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Home
